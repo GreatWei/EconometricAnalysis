22bd71e322f37c0dc26ee4054f3d90a273b1c3ed
GreatWei <1121412775@qq.com>
2021-04-20 00:36:15 +0800

    LSTM

--- MyLSTM.py
+++ MyLSTM.py
@@ -17,10 +17,39 @@ import matplotlib.pyplot as plt
 # setting figure size
 from matplotlib.pylab import rcParams
 
-rcParams['figure.figsize'] = 20,10
+rcParams['figure.figsize'] = 20, 10
 
 # for normalizing data
 from sklearn.preprocessing import MinMaxScaler
+
+
+def trainModel(x_train, y_train):
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+    model.add(LSTM(units=50))
+    model.add(Dense(1))
+
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(x_train, y_train, epochs=1, batch_size=1, verbose=2)
+    return model
+
+
+def predictval(model, original_data ,leng, scaler):
+    all_closing_price =[]
+    for i in range(0,leng):
+        inputs=scaler.transform(original_data)
+        X_test = []
+        X_test.append(inputs)
+        X_test = np.array(X_test)
+        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+        closing_price = model.predict(X_test)
+        tmp = scaler.inverse_transform(closing_price)
+        all_closing_price.append(tmp)
+        original_data = np.append(original_data, tmp)
+        original_data = original_data.reshape(-1, 1)
+        original_data = original_data[1:,]
+
+    return np.array(all_closing_price)
 
 scaler = MinMaxScaler(feature_range=(0, 1))
 
@@ -33,8 +62,6 @@ df.head()
 # setting index as date
 df['Date'] = pd.to_datetime(df.Date, format='%Y-%m-%d')
 df.index = df['Date']
-
-
 
 # creating dataframe
 data = df.sort_index(ascending=True, axis=0)
@@ -54,9 +81,9 @@ print("new_data===================================")
 dataset = new_data.values
 # print(dataset)
 train = dataset[0:987, :]
-print("train: ",len(train))
+print("train: ", len(train))
 valid = dataset[987:, :]
-print("valid: ",len(valid))
+print("valid: ", len(valid))
 # converting dataset into x_train and y_train
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(dataset)
@@ -81,14 +108,14 @@ x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 # predicting 246 values, using past 60 from the train data
 #                    2035               987
-print("input start:",len(new_data) - len(valid) - 60)
+print("input start:", len(new_data) - len(valid) - 60)
+
 inputs = new_data[len(new_data) - len(valid) - 60:].values
-print("input values",inputs)
 inputs = inputs.reshape(-1, 1)
 inputs = scaler.transform(inputs)
 print("X_test===================================")
 X_test = []
-print("input :",inputs.shape)
+print("input :", inputs.shape)
 for i in range(60, inputs.shape[0]):
     X_test.append(inputs[i - 60:i, 0])
 X_test = np.array(X_test)
@@ -101,31 +128,27 @@ print(X_test.shape)
 
 
 # create and fit the LSTM network
-model = Sequential()
-model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(LSTM(units=50))
-model.add(Dense(1))
 
-model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(x_train, y_train, epochs=1, batch_size=1, verbose=2)
 print("closing_price===========================================")
 print(X_test.shape)
 
+model = trainModel(x_train, y_train)
 closing_price = model.predict(X_test)
 closing_price = np.array(closing_price)
-print(closing_price)
+print(closing_price[0][0])
 print(closing_price.shape)
 print("===========================================")
 closing_price = scaler.inverse_transform(closing_price)
 
 # for plotting
 train = new_data[:987]
-valid = new_data[987:987+len(closing_price)]
+valid = new_data[987:987 + len(closing_price)]
 valid['Predictions'] = closing_price
 # valid['MyPredictions'] = myclosing_price
-#plt.plot(new_data,label='new_data')
-ax=plt.gca()
+# plt.plot(new_data,label='new_data')
+ax = plt.gca()
 from matplotlib.dates import AutoDateLocator, DateFormatter, DayLocator
+
 # # 设置x轴主刻度格式
 # alldays = mdates.DayLocator()  # 主刻度为每天
 # ax1.xaxis.set_major_locator(alldays)  # 设置主刻度
@@ -142,10 +165,9 @@ alldays = DayLocator()  # 主刻度为每天
 ax.xaxis.set_major_locator(alldays)
 yearsFmt = DateFormatter('%Y-%m-%d')
 ax.xaxis.set_major_formatter(yearsFmt)
-print("train.index:",train.index)
-print("valid.index:",valid.index)
-plt.plot(valid['Close'],label='Close')
-plt.plot(valid['Predictions'],label='Predictions')
+print("train.index:", train.index)
+print("valid.index:", valid.index)
+plt.plot(valid['Close'], label='Close')
+plt.plot(valid['Predictions'], label='Predictions')
 plt.legend(loc='best')
 plt.show()
-
---
+++ Test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+tmp =[[12],[456]]
+tmp = np.array(tmp)
+tmp = tmp.reshape(-1, 1)
+#print(tmp)
+tmp=np.append(tmp,np.array([[11]]))
+tmp = tmp.reshape(-1, 1)
+print(tmp[1:,])
+
+
