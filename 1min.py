d4a3bef6e01487e49e96d227f93f904a21836445
GreatWei <1121412775@qq.com>
2021-04-29 01:50:33 +0800

    1

---
+++ 1min.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import numpy as np
+
+# to plot within notebook
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+from pandas import Timestamp
+
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# setting figure size
+from matplotlib.pylab import rcParams
+
+rcParams['figure.figsize'] = 20, 10
+
+# for normalizing data
+from sklearn.preprocessing import MinMaxScaler
+
+
+def trainModel(x_train, y_train):
+    model = Sequential()
+    # model.add(LSTM(units=150, return_sequences=True, input_shape=(x_train.shape[1], 7)))
+    # model.add(LSTM(units=150))
+    # model.add(LSTM(units=270, return_sequences=True, input_shape=(x_train.shape[1], 7)))
+    # model.add(LSTM(units=270))
+    model.add(LSTM(units=150, return_sequences=True, input_shape=(x_train.shape[1], 7)))
+    model.add(LSTM(units=150))
+    model.add(Dense(7))
+
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    #model.fit(x_train, y_train, epochs=4, batch_size=20, verbose=2)
+    #model.fit(x_train, y_train, epochs=4, batch_size=30, verbose=2)
+    model.fit(x_train, y_train, epochs=6, batch_size=7, verbose=2)
+    return model
+
+
+def predictval(model, original_data, leng, scaler):
+    all_closing_price = []
+    original_data_cp = original_data
+    for i in range(0, leng):
+        # print("i:",i)
+        X_test = []
+        inputs = scaler.transform(original_data_cp)
+        # print(inputs.shape)
+        for j in range (0,i+1):
+            X_test.append(inputs[j:(j + 60), ])
+        X_test = np.array(X_test)
+        # print(X_test.shape)
+        closing_price = model.predict(X_test)
+        tmp = scaler.inverse_transform(closing_price)
+        all_closing_price = tmp
+        original_data_cp = np.append(original_data, tmp)
+        original_data_cp = original_data_cp.reshape(-1, 7)
+
+    return all_closing_price
+
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+# read the file
+df = pd.read_csv('1minethusdt.csv')
+df.head()
+print(df)
+df.index = df['Id']
+data = df.sort_index(ascending=True, axis=0)
+data.drop('Id', axis=1, inplace=True)
+print(data)
+dataset=data.values
+print("dataset",dataset.shape)
+train=dataset[0:1500,:]
+valid = dataset[1500:, :]
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
+print(scaled_data.shape)
+
+x_train, y_train = [], []
+for i in range(60, len(train)):
+    x_train.append(scaled_data[i - 60:i, ])
+    y_train.append(scaled_data[i, ])
+
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+print("x_train",x_train.shape)
+print("x_train",x_train)
+print("y_train",y_train.shape)
+
+inputs = dataset[1440 : , : ]
+original_data = inputs[0:60]
+leng = inputs.shape[0] - 60
+inputs = scaler.transform(inputs)
+X_test = []
+print("inputs :", inputs.shape)
+for i in range(60, inputs.shape[0]):
+    X_test.append(inputs[i - 60:i, ])
+X_test = np.array(X_test)
+
+
+# create and fit the LSTM network
+model = trainModel(x_train, y_train)
+
+my_closing_price = predictval(model, original_data, leng, scaler)
+my_closing_price = np.array(my_closing_price)
+print("closing_price===========================================")
+print(X_test.shape)
+
+closing_price = model.predict(X_test)
+closing_price = np.array(closing_price)
+closing_price = scaler.inverse_transform(closing_price)
+
+
+
+
+# print("closing_price",closing_price)
+# print("closing_price",closing_price[:,3])
+
+train = data[:1500]['Close']
+valid = data[1500:1500 + len(closing_price)]
+valid['Predictions'] = closing_price[:,3]
+valid['MyPredictions'] = my_closing_price[:,3]
+# plt.plot(train, label='train_Close')
+plt.plot(valid['Close'], label='Close')
+plt.plot(valid['Predictions'], label='Predictions')
+plt.plot(valid['MyPredictions'], label='MyPredictions')
+plt.legend(loc='best')
+plt.show()
